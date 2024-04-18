853801ca533259ac27414bdb19fd651131cb3823
ooMatter <Tyrice02@gmail.com>
2024-04-18 19:39:27 -0400

    edited email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,13 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-// import emailjs from "@emailjs/browser";
-
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import axios from "axios";
+
+const user = process.env.SMPTU;
+const pass = process.env.SMPTP;
 
 const Contact = () => {
   const formRef = useRef();
@@ -31,39 +33,39 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
-        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    axios
+      .post(
+        "https://api.hostinger.com/v1/email",
         {
-          from_name: form.name,
-          to_name: "Tyrice Freeman",
-          from_email: form.email,
-          to_email: "contact@tyricefreeman.com",
-          message: form.message,
+          from: form.email,
+          to: "contact@tyricefreeman.com",
+          subject: `New message from ${form.name}`,
+          text: form.message,
         },
-        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
-
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Ahh, something went wrong. Please try again.");
+        {
+          auth: {
+            username: user,
+            password: pass,
+          },
         }
-      );
-  };
+      )
+      .then(() => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
 
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+
+        alert("Ahh, something went wrong. Please try again.");
+      });
+  };
   return (
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
